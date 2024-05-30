3f3b9b0a09babe840ab7f381ed3d181c98f8eb43
Simon Busch <Simon.busch@live.fr>
2024-05-30 06:06:17 +0200

    add fuzzer files

---
+++ test/recon/TargetFunctions.sol
@@ -0,0 +1,19 @@
+
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.0;
+
+import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
+import {BeforeAfter} from "./BeforeAfter.sol";
+import {Properties} from "./Properties.sol";
+import {vm} from "@chimera/Hevm.sol";
+
+abstract contract TargetFunctions is BaseTargetFunctions, Properties, BeforeAfter {
+
+    function counter_increment() public {
+      counter.increment();
+    }
+
+    function counter_setNumber(uint64 newNumber) public {
+      counter.setNumber(newNumber);
+    }
+}
