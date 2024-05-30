c882c450ec800d8e0bf15b015cf071843cc9e2f4
Simon Busch <Simon.busch@live.fr>
2024-05-30 06:04:33 +0200

    change uint type

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    uint64 public number;
 
-    function setNumber(uint256 newNumber) public {
+    function setNumber(uint64 newNumber) public {
         number = newNumber;
     }
 
